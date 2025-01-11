2046cd212ea8891c188694813fd5f46c47b57d7c
ffayad <ffayad@ptcdb.edu.ps>
2025-01-11 11:42:54 +0200

    more specific error handling

--- simple_execption.py
+++ simple_execption.py
@@ -1,6 +1,11 @@
+from traceback import print_tb
+
 try:
     x = int(input("Enter a number: "))
     y = int(input("Enter another number: "))
     print(x / y)
-except Exception as e:
-    print(f"Error occured {e}")
+except ValueError as e:
+    print(f"You should input a number: {e}")
+except ZeroDivisionError as e:
+    print(f"You cannot divide by zero: {e}")
+print("Thank you!")
